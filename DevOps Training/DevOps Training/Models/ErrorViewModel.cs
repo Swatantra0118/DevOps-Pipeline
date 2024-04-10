90251511492322aa284d7d43ee291aa50ee2c4a6
Swatantra Tiwari <swatantratiwari057@gmail.com>
2024-04-10 14:27:52 +0530

    Demo Project For DevOps Pipeline

---
+++ DevOps Training/DevOps Training/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevOps_Training.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
